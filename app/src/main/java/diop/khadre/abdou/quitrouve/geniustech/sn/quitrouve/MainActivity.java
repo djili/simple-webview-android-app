f320f5423bfeabc8fb969a39274e329f7c954e15
djili <sndevnb1@gmail.com>
2017-06-04 00:17:02 +0000

    first commit

---
+++ app/src/main/java/diop/khadre/abdou/quitrouve/geniustech/sn/quitrouve/MainActivity.java
@@ -0,0 +1,139 @@
+package diop.khadre.abdou.quitrouve.geniustech.sn.quitrouve;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+    ProgressBar progressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        String url="https://www.quitrouve.com";
+        mWebView.loadUrl(url);
+        mWebView.setWebViewClient(new HelloWebViewClient());
+
+
+    }
+
+    private class HelloWebViewClient extends WebViewClient{
+
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        /*public void onReceivedError(WebView webView, int errorCode, String description, String failingUrl) {
+            try {
+                webView.stopLoading();
+            } catch (Exception e) {
+            }
+
+            if (webView.canGoBack()) {
+                webView.goBack();
+            }
+
+            webView.loadUrl("about:blank");
+            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+            alertDialog.setTitle("Erreur");
+            alertDialog.setMessage("Verifier votre connexion internet.");
+            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Try Again", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                    startActivity(getIntent());
+                }
+            });
+
+            alertDialog.show();
+            super.onReceivedError(webView, errorCode, description, failingUrl);
+        }*/
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webView, String url)
+        {
+            webView.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // TODO Auto-generated method stub
+            super.onPageFinished(view, url);
+
+            progressBar.setVisibility(view.GONE);
+        }
+
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    { //if back key is pressed
+        if((keyCode == KeyEvent.KEYCODE_BACK)&& mWebView.canGoBack())
+        {
+            mWebView.goBack();
+            return true;
+
+        }
+
+        return super.onKeyDown(keyCode, event);
+
+    }
+
+
+
+
+
+    public void onBackPressed() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                MainActivity.this);
+
+        // set title
+        alertDialogBuilder.setTitle("Sortie");
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage("Voulez-vous vraiment sortir?")
+                .setCancelable(false)
+                .setPositiveButton("Oui",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, close
+                        // current activity
+                        MainActivity.this.finish();
+                    }
+                })
+                .setNegativeButton("Non",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, just close
+                        // the dialog box and do nothing
+                        dialog.cancel();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+}
